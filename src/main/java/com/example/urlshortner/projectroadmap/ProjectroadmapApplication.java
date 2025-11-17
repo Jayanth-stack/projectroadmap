6a5396ae95953cf438d7e3a6e5fff46a519e6fb6
Jayanth-stack <jayanthalapati@gmail.com>
2025-11-16 19:45:26 -0500

    initial commit

---
+++ src/main/java/com/example/urlshortner/projectroadmap/ProjectroadmapApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlshortner.projectroadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectroadmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectroadmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/urlshortner/projectroadmap/ProjectroadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlshortner.projectroadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectroadmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
